e3bbf77e56b56932fca4288a50f484e7047b5df3
ArthurHlt <arthurh.halet@gmail.com>
2015-04-11 19:04:57 +0200

    switch to new sphring

--- src/ArthurH/SphringCache/SphringCacheRunnerPlugin.php
+++ src/ArthurH/SphringCache/SphringCacheRunnerPlugin.php
@@ -1,13 +1,11 @@
 <?php
 namespace ArthurH\SphringCache;
 
-use Arthurh\Sphring\Annotations\AnnotationsSphring\LoadContext;
 use Arthurh\Sphring\Annotations\AnnotationsSphring\RootProject;
 use ArthurH\SphringCache\CacheManager\AbstractCacheManager;
 
 /**
  * Class SphringRunnerPlugin
- * @LoadContext
  * @RootProject(file="../../../")
  */
 class SphringCacheRunnerPlugin extends \Arthurh\Sphring\Runner\SphringRunner
